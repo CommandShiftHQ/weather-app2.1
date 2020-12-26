e28327bd6cf91897ff5581e827333fb457e01b42
Martyna Machowiak <martyna.machowiak@gmail.com>
2020-12-26 23:03:17 +0000

    Add markup for each forecast detail in <ForecastSummary />

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -3,7 +3,18 @@ import PropTypes from "prop-types";
 
 const ForecastSummary = (props) => {
   const { date, icon, temperature, description } = props;
-  return <div className="forecast-summary"></div>;
+
+  return (
+    <div className="forecast-summary">
+      <div className="forecast-summary__date">{date}</div>
+      <div className="forecast-summary__icon">{icon}</div>
+      <div className="forecast-summary__temperature">
+        {temperature.max}
+        &deg;C
+      </div>
+      <div className="forecast-summary__description">{description}</div>
+    </div>
+  );
 };
 
 export default ForecastSummary;
