21538c46dfe4bf841e3c88da418a7bdb9102c65d
Martyna Machowiak <martyna.machowiak@gmail.com>
2020-12-27 20:05:22 +0000

    Pass forecasts to <App />

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./styles/index.css";
-import { location } from "./data/forecast.json";
+import { forecasts, location } from "./data/forecast.json";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App location={location} />
+    <App forecasts={forecasts} location={location} />
   </React.StrictMode>,
   document.getElementById("root")
 );
