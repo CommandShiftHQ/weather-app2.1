7bf333ee0c8039085923379a72b74669255c0171
Martyna Machowiak <martyna.machowiak@gmail.com>
2020-12-27 21:35:41 +0000

    Create test file for <ForecastSummaries />  with validProps

---
+++ src/tests/components/ForecastSummaries.test.js
@@ -0,0 +1,22 @@
+describe("ForecastSummaries", () => {
+  const validProps = [
+    {
+      date: 1111111,
+      description: "Stub description 1",
+      icon: "stubIcon1",
+      temperature: {
+        max: 22,
+        min: 12,
+      },
+    },
+    {
+      date: 2222222,
+      description: "Stub description2",
+      icon: "stubIcon2",
+      temperature: {
+        max: 24,
+        min: 13,
+      },
+    },
+  ];
+});
