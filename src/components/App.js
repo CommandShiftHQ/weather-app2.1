b47a7f55832351c17f065a94fdaf19f77a3099c6
Martyna Machowiak <martyna.machowiak@gmail.com>
2020-12-27 19:55:22 +0000

    Use <ForecastSummaries /> in <App />

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "../styles/App.css";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
 const App = (props) => {
   const { location } = props;
   return (
     <div className="App">
       <LocationDetails city={location.city} country={location.country} />
+      <ForecastSummaries />
     </div>
   );
 };
