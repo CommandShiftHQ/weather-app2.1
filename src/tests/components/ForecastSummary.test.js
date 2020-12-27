4a91ecf8256ed436fe3177ee43ffda2bf68fe914
Martyna Machowiak <martyna.machowiak@gmail.com>
2020-12-27 01:17:29 +0000

    Render <ForecastSummary /> component for tests

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummary from "../../components/ForecastSummary";
+
 describe("ForecastSummary", () => {
   const validProps = {
     date: 1111111,
@@ -9,5 +13,14 @@ describe("ForecastSummary", () => {
     },
   };
 
-  it("renders correctly", () => {});
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+  });
 });
