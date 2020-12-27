6f97b2a6d3e5f49fb335d964cd668b3889281b66
Martyna Machowiak <martyna.machowiak@gmail.com>
2020-12-27 01:15:19 +0000

    Add valid props to tests <ForecastSummary /> component

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -1,3 +1,13 @@
 describe("ForecastSummary", () => {
+  const validProps = {
+    date: 1111111,
+    description: "Stub description",
+    icon: "stubIcon",
+    temperature: {
+      min: 12,
+      max: 22,
+    },
+  };
+
   it("renders correctly", () => {});
 });
