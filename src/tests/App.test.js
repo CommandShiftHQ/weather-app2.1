c2a7dee053e5a7964cd2989ea2fe3364d36e2877
Martyna Machowiak <martyna.machowiak@gmail.com>
2020-12-28 01:36:35 +0000

    Fix test for <App />

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,11 +4,16 @@ import App from "../components/App";
 describe("App", () => {
   it("renders city and country", () => {
     const validProps = {
-      city: "MockCity",
-      country: "MockCountry",
+      location: {
+        city: "MockCity",
+        country: "MockCountry",
+      },
+      forecasts: [],
     };
 
-    render(<App location={validProps} />);
+    render(
+      <App forecasts={validProps.forecasts} location={validProps.location} />
+    );
     const copy = screen.getByText(/MockCity, MockCountry/i);
     expect(copy).toBeInTheDocument();
   });
