a5240f911dfc4c00f81d68aed788ea6cf1c28a78
Martyna Machowiak <martyna.machowiak@gmail.com>
2021-01-03 03:14:24 +0000

    Delete mock forecast data and its usage from <App />

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./styles/index.css";
-import { forecasts, location } from "./data/forecast.json";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App forecasts={forecasts} location={location} />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
