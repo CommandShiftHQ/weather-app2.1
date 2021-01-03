bafb160831bb6dd427cf0ddb079cac80805d23a8
Martyna Machowiak <martyna.machowiak@gmail.com>
2021-01-03 21:30:01 +0000

    Set default value for selectedDate state to 0

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import ForecastDetails from "./ForecastDetails";
 const App = () => {
   const [forecasts, setForecasts] = useState([]);
   const [location, setLocation] = useState({ city: "", country: "" });
-  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
+  const [selectedDate, setSelectedDate] = useState(0);
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate
   );
